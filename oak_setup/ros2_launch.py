306e924e963d6e16dad6e68fe3a9778a33c8224b
Ines Boujnah <inesboujnah@gmail.com>
2025-11-24 23:18:44 +0100

    added calibration commands and adjusted oak launch

--- oak_setup/ros2_launch.py
+++ oak_setup/ros2_launch.py
@@ -6,25 +6,43 @@ import os
 
 def generate_launch_description():
 
-    rgb_stereo_node = os.path.join(
-        get_package_share_directory('depthai_examples'),
+    oak_launch_node = os.path.join(
+        get_package_share_directory('depthai_ros_driver'),
         'launch',
-        'rgb_stereo_node.launch.py'
+        'camera.launch.py'
     )
-
-    stereo_inertial_node = os.path.join(
-        get_package_share_directory('depthai_examples'),
-        'launch',
-        'stereo_inertial_node.launch.py'
-    )
-
+ 
     return LaunchDescription([
-        IncludeLaunchDescription(PythonLaunchDescriptionSource(rgb_stereo_node)),
-        IncludeLaunchDescription(PythonLaunchDescriptionSource(stereo_inertial_node)),
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(oak_launch_node),
+            launch_arguments={
+                'camera.i_tf_camera_name': 'rgbd',
+                'camera.i_tf_camera_model': 'OAK-D-PRO-W',
+                'camera.i_pipeline_type': 'RGBD',
+                'camera.i_enable_sync': 'true',
+                'pipeline_gen.i_enable_sync': 'true'
+            }.items()
+        ),
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(oak_launch_node),
+            launch_arguments={
+                'camera.i_tf_camera_name': 'stereo',
+                'camera.i_tf_camera_model': 'OAK-D-PRO-W',
+                'camera.i_pipeline_type': 'Stereo',
+                'camera.i_enable_sync': 'true',
+                'pipeline_gen.i_enable_sync': 'true',
+                'stereo.i_align_depth': 'true'
+            }.items()
+        ),
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(oak_launch_node),
+            launch_arguments={
+                'camera.i_tf_camera_name': 'stereo-inertial',
+                'camera.i_tf_camera_model': 'OAK-D-PRO-W',
+                'camera.i_pipeline_type': 'Stereo',
+                'camera.i_enable_sync': 'true',
+                'pipeline_gen.i_enable_sync': 'true',
+                'stereo.i_align_depth': 'true',
+                'pipeline_gen.i_enable_imu': 'true'
+            }.items()
+        ),
     ])
-
-# ros2 launch depthai_examples rgb_stereo_node.launch.py
-
-# ros2 launch depthai_examples stereo_inertial_node.launch.py
 
 # ros2 launch depthai_ros_driver camera.launch.py
