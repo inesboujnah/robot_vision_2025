669b0e1bd4a419470d27c6a680203aa9882b7b4b
Ines Boujnah <inesboujnah@gmail>
2025-12-04 17:14:53 +0100

    adjustde entrypoint so that recording is in background and doesnt affect launch

--- realsense_setup/realsense_entrypoint_with_bag.sh
+++ realsense_setup/realsense_entrypoint_with_bag.sh
@@ -9,15 +9,12 @@ source /root/colcon_ws/install/setup.bash
 
 cd /root/memory_register/realsense
 
-# If RECORD_BAG is set to 1 or true, start ros2 bag recording in background
 if [ "${RECORD_BAG}" = "1" ] || [ "${RECORD_BAG,,}" = "true" ]; then
     
-    # record into a timestamped subfolder
     TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
     BAG_OUTPUT="/realsense_${TIMESTAMP}"
     echo "Starting ros2 bag record -a -> ${BAG_OUTPUT}"
-    
-    ros2 bag record -a -o "$BAG_OUTPUT"
+    ros2 bag record -a -o "$BAG_OUTPUT" & 
 fi
 
-exec ros2 launch launch_manager ros2_launch.launch.py mode:=${MODE}
+exec ros2 launch launch_manager ros2_launch.launch.py mode:=${MODE} -d
