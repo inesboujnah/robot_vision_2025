1d09356c0438c5fe3d42d9690b2766e0923d972c
Ines Boujnah <inesboujnah@gmail>
2025-12-12 14:55:39 +0100

    - added mapping for memory registzer - wrong file path in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ source /opt/ros/humble/setup.bash
 source /root/colcon_ws/install/setup.bash
 
 if [ "${MODE}" = "stereo" ] || [ "${MODE}" = "stereo-inertial" ]; then
-    exec ros2 bag play /root/memory_register/${MODE}/${BAG} && exec ros2 run ${MODE} ./ORB_SLAM3_ROS2/Vocabulary/ORBvoc.txt ./config/${CAMERA}_${MODE}.yaml ${DO_RECTIFY}
+    exec ros2 bag play /root/memory_register/${CAMERA}/${BAG} && exec ros2 run ${MODE} ./ORB_SLAM3_ROS2/Vocabulary/ORBvoc.txt ./config/${CAMERA}_${MODE}.yaml ${DO_RECTIFY}
 else
-    exec ros2 bag play /root/memory_register/${MODE}/${BAG} && exec ros2 run ${MODE} ./ORB_SLAM3_ROS2/Vocabulary/ORBvoc.txt ./config/${CAMERA}_${MODE}.yaml
+    exec ros2 bag play /root/memory_register/${CAMERA}/${BAG} && exec ros2 run ${MODE} ./ORB_SLAM3_ROS2/Vocabulary/ORBvoc.txt ./config/${CAMERA}_${MODE}.yaml
 fi    
